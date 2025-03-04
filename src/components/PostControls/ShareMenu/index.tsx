40b6d0478bbd02d8fe527f3fc2470ac3ad10a3b0
Samuel Newman <mozzius@protonmail.com>
2025-03-04 13:24:58 +0000

    open native share menu on long press

--- src/components/PostControls/ShareMenu/index.tsx
+++ src/components/PostControls/ShareMenu/index.tsx
@@ -1,15 +1,20 @@
-import React, {memo, useMemo, useState} from 'react'
+import {memo, useCallback, useMemo, useState} from 'react'
 import {
   AppBskyFeedDefs,
   AppBskyFeedPost,
   AppBskyFeedThreadgate,
+  AtUri,
   RichText as RichTextAPI,
 } from '@atproto/api'
 import {msg} from '@lingui/macro'
 import {useLingui} from '@lingui/react'
 
+import {makeProfileLink} from '#/lib/routes/links'
+import {shareUrl} from '#/lib/sharing'
+import {toShareUrl} from '#/lib/strings/url-helpers'
 import {Shadow} from '#/state/cache/post-shadow'
 import {EventStopper} from '#/view/com/util/EventStopper'
+import {native} from '#/alf'
 import {ArrowOutOfBox_Stroke2_Corner0_Rounded as ArrowOutOfBoxIcon} from '#/components/icons/ArrowOutOfBox'
 import {useMenuControl} from '#/components/Menu'
 import * as Menu from '#/components/Menu'
@@ -53,6 +58,15 @@ let ShareMenuButton = ({
     }),
     [menuControl, setHasBeenOpen],
   )
+
+  const onNativeLongPress = useCallback(() => {
+    const urip = new AtUri(post.uri)
+    const href = makeProfileLink(post.author, 'post', urip.rkey)
+    const url = toShareUrl(href)
+    shareUrl(url)
+    onShare()
+  }, [post, onShare])
+
   return (
     <EventStopper onKeyDown={false}>
       <Menu.Root control={lazyMenuControl}>
@@ -63,7 +77,8 @@ let ShareMenuButton = ({
                 testID="postShareBtn"
                 big={big}
                 label={props.accessibilityLabel}
-                {...props}>
+                {...props}
+                onLongPress={native(onNativeLongPress)}>
                 <PostControlButtonIcon icon={ArrowOutOfBoxIcon} />
               </PostControlButton>
             )
